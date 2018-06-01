4920135900d594095f57924a5fb992142178b7d6
Reed Xia <huaqin2005@gmail.com>
2018-06-01 14:16:30 +0800

    add search and filter functions in admin page

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 A Web project based on Django, used for Web programming, Web API testing etc.
 
-Installed app:
+Installed apps:
 
 - sign
 
 Commands:
 
 - `python3 manage.py runserver` or `python3 manage.py runserver 127.0.0.1:8001` to launch the site.
+- `python3 manage.py shell` to enable Django shell console
--- sign/admin.py
+++ sign/admin.py
@@ -5,10 +5,14 @@ from sign.models import Guest
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'status', 'address', 'start_time']
+    search_fields = ['name']
+    list_filter = ['status']
 
 
 class GuestAdmin(admin.ModelAdmin):
     list_display = ['realname', 'phone', 'email', 'sign', 'create_time', 'event']
+    search_fields = ['realname', 'phone']
+    list_filter = ['sign']
 
 
 # Register your models here.
